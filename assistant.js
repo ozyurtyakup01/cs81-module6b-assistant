db1e80e593ca16f993a8cd3d2c384d694a0b8c65
ozyurtyakup01 <ozyurt_yakup01@student.smc.edu>
2025-07-12 15:28:43 -0700

    functions tested

--- assistant.js
+++ assistant.js
@@ -2,46 +2,53 @@ function PersonalAssistant(name)
 {
     this.name = name;
     this.tasks = [];
-    this.mood = "";
+    this.mood = "neutral";
 }
 
 PersonalAssistant.prototype.addTask = function (task)
 {
     this.tasks.push(task);
+    console.log(task + " added to the list")
 }
 
 PersonalAssistant.prototype.completeTask = function ()
 {
-    console.log(this.tasks[0] + " is completed"+ "\nNext: ")
+    console.log(this.tasks[0] + " is completed");
     this.tasks.shift();
-    console.log(this.tasks[0]);
+    console.log("New task is: " + this.tasks[0]);
 }
 
 PersonalAssistant.prototype.reportMood = function ()
 {
-    return this.mood;
+    console.log("Current Mood: " + this.mood);
 }
 
 PersonalAssistant.prototype.setMood = function (mood)
 {
     this.mood = mood;
+    console.log("Current Mood set to: " + this.mood);
 }
 
 PersonalAssistant.prototype.getList = function ()
 {
-    console.log("Todo:", this.tasks.join("\n- "))
+    console.log(this.tasks.length + " tasks left...")
+    console.log("TODO list \n-", this.tasks.join("\n- "));
 }
 
 //test
-let pa = new PersonalAssistant(Yakup);
+let pa = new PersonalAssistant("Yakup");
 
+pa.reportMood();
 pa.addTask("Homework 1");
+pa.getList();
 pa.addTask("CS81 QUIZ 6");
 pa.addTask("Laundry");
-pa.getList();
 pa.addTask("Grocery");
+pa.getList();
+pa.completeTask();
+pa.completeTask();
+pa.getList();
 pa.setMood("Happy");
 pa.reportMood();
-pa.completeTask();
-pa.completeTask();
-pa.getList();
+
+
